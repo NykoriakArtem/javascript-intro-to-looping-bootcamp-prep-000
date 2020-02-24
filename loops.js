98a8880895564d8d65f6b8d92f21aca436f18a59
Artem Nykoriak <artemnykoriak@gmail.com>
2020-02-24 15:47:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,7 +4,14 @@ function forLoop(array){
     array.push('I am 1 strange loop.')
   } else {
     array.push(`I am ${i} strange loops.`)
-  }
+    }
   }
   return array;
+}
+
+function whileLoop(n){
+  while (n > 0) {
+    console.log(n)
+    n += 
+  }
 }
